11cb57427de888dd234f61c44fdc7e617194c7a1
Ed ODonnell <ed.odonnell@infocarehealth.com>
2021-04-20 13:06:26 +0100

    tidy up time

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
 ## Repo : https://github.com/Ed-o/s3tohtml
 ## 
 
+# This file has any config items for you to change like bucket name
 require 'config.php';
-require 'vendor/aws-autoloader.php';
 
 # We will be using the AWS S3 PHP SDK V3 
 # (see the readme file for some more info on this)
+require 'vendor/aws-autoloader.php';
 use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
 
@@ -50,21 +51,26 @@ try {
 		header("Content-Type: {$result['ContentType']}");
 		# And retrun the contents of the file
 		echo $result['Body'] . "\n";
+
 	} else { # there is no file so lets assume this is a directory and display that for browsing
+
 		# The directory browser didn't work if the path was '/' so we remove that if we are looking at the root
 		if ( $url == "/" ) { $url = ""; }
 		# Pull the list of directories and files from the location in $url
 		$result = $s3Client->ListObjects(['Bucket' => "$bucket", 'Delimiter'=>'/', 'Prefix' => "$url"]);
+
 		# Add the HTML for the page and table layout
 		echo "<html><head><title>S3toHTML</title><link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\"></head><body>";
 		echo "<h3>S3toHTML browser</h3><br /><br />";
 		echo "<table id=\"cols\">";
 		echo "<tr><th colspan=\"2\"><center>$bucket</center></th></tr>";
+
 		# Start be displaying the directory names and make them into links so we can browse round
 		foreach ($result["CommonPrefixes"] as $res) {
 			echo "<tr><td><a href=\"/" . $res["Prefix"] . "\"><img src=\"/images/folder.png\"></a></td>";
 			echo "<td><a href=\"/" . $res["Prefix"] . "\">" . $res["Prefix"] . "</a></td></tr>";
 		}
+
 		# and then if there are files inside this directory then show them as links too 
 		if (isset ($result[@Contents])) {
 			foreach ($result["Contents"] as $res) {
@@ -83,6 +89,4 @@ try {
 } catch (Aws\S3\Exception\S3Exception $e) {
 	echo $e->getMessage() . "\n";
 }
-
 ?>
-
